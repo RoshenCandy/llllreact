a63f438b3fdcc796cb65ba429296848caeed0179
RoshenCandy <47426876+RoshenCandy@users.noreply.github.com>
2019-03-01 17:08:41 +0200

    llll

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './images/logo.jpg'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="color">
+	<div className="container">
+		<div className="top-header">
+			<div className="embl"><a href="#"><h1>Decision mapper</h1></a></div>
+			<nav>
+				<ul class="buttons">
+					<a href="#"><li>work</li></a>
+					<a href="#"><li>services</li></a>
+					<a href="#"><li>about</li></a>
+					<a href="#"><li>tutorials</li></a>
+					<a href="#"><li className="contact">contact</li></a>
+				</ul>
+			</nav>
+		</div>
+		<div className="lingua_center_text">
+			<h1>Lingua.Live</h1>
+			<p>Helping language learners grow through conversation with native speakers</p>
+		</div>
+	</div>
+</div>
+</header>
+
+</div>
     );
   }
 }
